41468261a518fe9b19a582499b2adbb0f165e033
chris <cdresher2010@gmail.com>
2022-08-29 18:36:31 -0400

    added create_squares and create_board functions to instantiate the board for the game. added check_player for basic game error checking

--- lib/tictac.ex
+++ lib/tictac.ex
@@ -1,18 +1,21 @@
 defmodule Tictac do
   @moduledoc """
-  Documentation for `Tictac`.
+  this module will hold the core game creation and logic for out tictac game
   """
 
-  @doc """
-  Hello world.
+  def check_player(player) do
+    case player do
+       :x -> {:ok, player}
+       :o -> {:ok, player}
+       _ -> {:error, :invalid_location}
+    end
+  end
 
-  ## Examples
+  def create_board do
+    for square <- create_squares(), into: %{}, do: {square, :empty}
+  end
 
-      iex> Tictac.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def create_squares do
+     for c <- 1..3, r <- 1..3, into: MapSet.new(), do: %Tictac.Square{col: c, row: r}
   end
 end
