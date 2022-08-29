4a4d489692e5e1eb4207705b11be191670cd5c38
chris <cdresher2010@gmail.com>
2022-08-28 21:29:54 -0400

    created square struct that will act as the key for our game board

--- lib/tictac/square.ex
+++ lib/tictac/square.ex
@@ -2,4 +2,10 @@ defmodule Tictac.Square do
   @enforce_keys [:row, :col]
 
   defstruct [:row, :col]
+
+  def new(col, row) when col in 1..3 and row in 1..3 do
+    {:ok, %Tictac.Square{row: row, col: col}}
+  end
+
+  def new(_col, _row), do: {:error, :invalid_square}
 end
