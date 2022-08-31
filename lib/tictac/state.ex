8968294e516c242f3c370ff2e1a8f93223a8af57
chris <cdresher2010@gmail.com>
2022-08-31 07:49:46 -0400

    built structure of State module

--- lib/tictac/state.ex
+++ lib/tictac/state.ex
@@ -1,3 +1,19 @@
 defmodule Tictac.State do
-  
+  @moduledoc """
+  this module will be a state machine that defines all possible states in the game and their allowed changes
+  """
+
+  alias Tictac.{Square, State}
+
+  @players [:x, :o]
+
+  defstruct status: :initial,
+                     over: false,
+                     turn: nil,
+                     winner: nil,
+                     board: Square.create_board(),
+                     ui: nil
+
+  def name(), do: {:ok, %State{}}
+  def name(ui), do: {:ok, %State{ui: ui}}
 end
