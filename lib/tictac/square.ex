c2c2b6d8167857509ec5599af778ace95bc9cec1
chris <cdresher2010@gmail.com>
2022-08-31 07:38:53 -0400

    refactored tictac and square to have square hold all code related to creating squares and boards. this also moves all hardcoded vars to the same module

--- lib/tictac/square.ex
+++ lib/tictac/square.ex
@@ -3,9 +3,18 @@ defmodule Tictac.Square do
 
   defstruct [:row, :col]
 
-  def new(col, row) when col in 1..3 and row in 1..3 do
+  @board_size 1..3
+
+  def new(col, row) when col in @board_size and row in @board_size do
     {:ok, %Tictac.Square{row: row, col: col}}
   end
-
   def new(_col, _row), do: {:error, :invalid_square}
+
+  def create_board do
+    for square <- create_squares(), into: %{}, do: {square, :empty}
+  end
+
+  def create_squares do
+     for c <- @board_size, r <- @board_size, into: MapSet.new(), do: %Tictac.Square{col: c, row: r}
+  end
 end
