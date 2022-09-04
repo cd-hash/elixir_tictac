297793d92287a28ccb8f18ea1e58c0c85495b067
chris <cdresher2010@gmail.com>
2022-09-04 08:02:16 -0400

    created all states and their correspoding transitions

--- lib/tictac/state.ex
+++ lib/tictac/state.ex
@@ -8,12 +8,56 @@ defmodule Tictac.State do
   @players [:x, :o]
 
   defstruct status: :initial,
-                     over: false,
                      turn: nil,
                      winner: nil,
                      board: Square.create_board(),
                      ui: nil
 
-  def name(), do: {:ok, %State{}}
-  def name(ui), do: {:ok, %State{ui: ui}}
+  def new(), do: {:ok, %State{}}
+  def new(ui), do: {:ok, %State{ui: ui}}
+
+  def event(%State{status: :initial} = state, {:choose_p1, player}) do
+    case Tictac.check_player player do
+      {:ok, p} -> {:ok, %State{state | status: :playing, turn: p}}
+      _ -> {:error, :invalid_player}
+    end
+  end
+
+  def event(%State{status: :playing}, {:play, p}) when p not in @players do
+    {:error, :invalid_player}
+  end
+
+  def event(%State{status: :playing, turn: p} = state, {:play, p}) do
+    {:ok, %State{state | turn: other_player(p)}}
+  end
+
+  def event(%State{status: :playing}, {:play, _}), do: {:error, :out_of_turn}
+
+  def event(%State{status: :playing} = state, {:check_for_winner, winner}) do
+    case winner do
+      :x -> {:ok, %State{state | status: :game_over, winner: winner}}
+      :o -> {:ok, %State{state | status: :game_over, winner: winner}}
+      _ -> {:error, :invalid_winner}
+    end
+  end
+
+  def event(%State{status: :playing} = state, {:game_over?, over_or_not}) do
+    case over_or_not do
+      :not_over -> {:ok, state}
+      :over -> {:ok, %State{state | status: :game_over, winner: :tie}}
+      _ -> {:error, :invalid_game_over_status}
+    end
+  end
+
+  def event(state, action) do
+    {:error, {:invalid_state_transition, %{status: state.status, action: action}}}
+  end
+
+  def other_player(current_player) do
+    case current_player do
+      :x -> :o
+      :o -> :x
+      _ -> {:error, :invalid_player}
+    end
+  end
 end
